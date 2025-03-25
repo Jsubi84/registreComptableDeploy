eec4a7c447a302a1906b9fa300b64fb3fb0b7cfe
Jordi <jorsubirana@gmail.com>
2025-03-25 22:02:07 +0100

    Cross issue

--- src/main/java/com/app/config/CORSConfig.java
+++ src/main/java/com/app/config/CORSConfig.java
@@ -14,7 +14,8 @@ public class CORSConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        //.allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT","DELETE", "OPTIONS")
                 		.allowedHeaders("*");
             }
