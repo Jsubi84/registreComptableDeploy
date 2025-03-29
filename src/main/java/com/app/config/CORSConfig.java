c6bea8da43bcd98f0cbfaa67ce4404b47a0a84c1
Jordi Subirana <jorsubirana@gmail.com>
2025-03-29 10:34:29 +0100

    Fix cors.

--- src/main/java/com/app/config/CORSConfig.java
+++ src/main/java/com/app/config/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig {
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
                         //.allowedOrigins("http://localhost:4200")
-                        .allowedOrigins("https://registrecomptableangular.onrender.com")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT","DELETE", "OPTIONS")
                 		.allowedHeaders("*");
             }
